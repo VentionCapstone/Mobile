2369cbf7075fbfac056e59d0bcf3e56f109d707d
VladimirNee <vova97nee@gmail.com>
2023-12-19 13:51:56 +0500

    fix: fixed styling

--- src/screens/auth/VerifyEmail/VerifyEmail.styles.ts
+++ src/screens/auth/VerifyEmail/VerifyEmail.styles.ts
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native';
-import { RED_200, RED_300, paragraph1 } from 'src/styles';
 
 export const styles = StyleSheet.create({
   container: {
@@ -9,23 +8,7 @@ export const styles = StyleSheet.create({
     flexGrow: 1,
     paddingHorizontal: 15,
   },
-  errorContainer: {
-    borderRadius: 8,
-    minHeight: 60,
-    backgroundColor: RED_300,
-    padding: 10,
-  },
-  label: {
-    fontWeight: '500',
-    color: RED_200,
-    ...paragraph1,
-  },
-  errorMessage: {
-    color: RED_200,
-    ...paragraph1,
-  },
-  errorIconContainer: {
-    flexDirection: 'row',
-    gap: 4,
+  text: {
+    fontSize: 30,
   },
 });
